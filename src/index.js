b22b1da720b493b799d563c8726675a6ceea2e7e
FND <fnd@localhost.localdomain>
2018-03-31 10:55:37 +0200

    improved performance by indexing charset
    this significantly reduces the performance gap to the original version
    (cf. previous commit; d36c757d37ab3b4eb0544bfeae383fdb21298f46), though
    it still seems to be a little slower
    
    memory overhead due to double indexing should be negligible

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
-let CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+let CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+CHARSET = index(CHARSET);
 
 // NB: does not validate input
 export function encode(int) {
+	let charset = CHARSET.byCode;
+
 	if(int === 0) {
-		return CHARSET[0];
+		return charset[0];
 	}
 
 	let res = "";
 	while(int > 0) {
-		res = CHARSET[int % 62] + res;
+		res = charset[int % 62] + res;
 		int = Math.floor(int / 62);
 	}
 	return res;
 }
 
 export function decode(str) {
+	let charset = CHARSET.byChar;
+
 	let res = 0;
 	let { length } = str;
 	for(let i = 0; i < length; i++) {
-		res += CHARSET.indexOf(str[i]) * (62 ** (length - i - 1));
+		let char = str[i];
+		res += charset[char] * (62 ** (length - i - 1));
 	}
 	return res;
+}
+
+function index(charset) {
+	return charset.split("").reduce((memo, char, i) => {
+		memo.byCode[i] = char;
+		memo.byChar[char] = i;
+		return memo;
+	}, {
+		byCode: {},
+		byChar: {}
+	});
 }
